ebad12a3fce6c3b7af87ae275adec4b51fddf0c7
johnyu0611 <93964026+johnyu0611@users.noreply.github.com>
2021-11-23 11:48:26 -0800

    Add files via upload

--- DQL.py
+++ DQL.py
@@ -5,19 +5,23 @@ import random
 import numpy as np
 from collections import deque
 import tensorflow as tf
-from tensorflow.keras.models import Sequetial
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from cs229_project_scenario import ObstacleAvoidanceScenario
+# from aa228_project_scenario import GoalFollowingScenario
+from matplotlib import pyplot as plt
 
 # Set up environment
 env = ObstacleAvoidanceScenario()
+# env = GoalFollowingScenario()
 
 # Hyperparameters
-state_size = env.observation_space().shape[0] # This has to be from (env.observation_space)
-action_size = len(env.action_space()) # This has to be from (env.action_space)
+state_size = 4 # This has to be from (env.observation_space)
+action_size = 3 # This has to be from (env.action_space)
 batch_size = 32
-n_episodes = 1001
+n_episodes = 3001
+dt = 0.1
 output_dir = "model_output/carlo_model"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
@@ -33,7 +37,7 @@ class DQNAgent:
         self.gamma = 0.95 # Discount factor, will be tuned
 
         self.epsilon = 1.0 
-        self.epsilon_decay = 0.995 # exploration rate decay, will be tuned
+        self.epsilon_decay = 0.998 # exploration rate decay, will be tuned
         self.epsilon_min = 0.01 # minimum exploration rate, will be tuned
 
         self.learning_rate = 0.001 # neural network learning rate, will be tuned
@@ -44,8 +48,8 @@ class DQNAgent:
 
         model = Sequential()
 
-        model.add(Dense(24, input_dim=self.state_size, activation="relu")) # First layer, will be tuned
-        model.add(Dense(24, activation="relu")) # Second layer, will be tuned
+        model.add(Dense(64, input_dim=self.state_size, activation="relu")) # First layer, will be tuned
+        model.add(Dense(32, activation="relu"))
         model.add(Dense(self.action_size, activation='linear'))
 
         model.compile(loss="mse", optimizer=Adam(lr=self.learning_rate))
@@ -87,13 +91,14 @@ agent = DQNAgent(state_size, action_size)
 
 ### interaction ###
 done = False
+plot_reward = []
 for e in range(n_episodes):
 
     state = env.reset()
     state = np.reshape(state, [1, state_size])
     total_reward = 0.
 
-    for time in range(5000):
+    for t in range(5000):
 
         env.render() # See the training process
 
@@ -105,20 +110,30 @@ for e in range(n_episodes):
 
         next_state = np.reshape(next_state, [1, state_size])
 
-        agent.remember(state, action, reward, next_state, done)
+        agent.remember(state, action_idx, reward, next_state, done)
 
         state = next_state
 
         total_reward += reward
-
+    
         if done:
             env.close()
+            plot_reward.append(total_reward)
             print("episode: {", e , "}/{" , n_episodes , "}, cumulative reward: {" , total_reward , "}, e: {" , agent.epsilon, "}")
             break 
-        
+
+        # if e <= 20:
+        #     time.sleep(dt/4)
+
     if len(agent.memory) > batch_size:
         agent.replay(batch_size)
 
     if e % 50 == 0:
-        agent.save(output_dir + "weights_" + ":04d".format(e) + ".hdf5")
+        agent.save(output_dir + "weights_" + str(e) + ".hdf5")
+
+plt.plot(plot_reward)
+plt.title("Total reward vs. Episode")
+plt.xlabel('Episode')
+plt.ylabel('Reward')
+plt.savefig('Reward_aa218')
         
